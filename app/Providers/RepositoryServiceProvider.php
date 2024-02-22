2026839aaa2c14ef22f707fb56ad2ac0986db2ac
julianNunes <eu_dinovu@hotmail.com>
2024-02-22 16:07:52 -0300

    finalizado repositores

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -4,12 +4,38 @@ namespace App\Providers;
 
 use App\Repositories\{
     AppRepository,
+    BudgetExpenseRepository,
+    BudgetGoalRepository,
+    BudgetIncomeRepository,
+    BudgetProvisionRepository,
+    BudgetRepository,
+    CreditCardInvoiceExpenseDivisionRepository,
+    CreditCardInvoiceExpenseRepository,
+    CreditCardInvoiceFileRepository,
+    CreditCardInvoiceRepository,
+    CreditCardRepository,
+    FinancingInstallmentRepository,
+    FinancingRepository,
+    FixExpenseRepository,
     ProvisionRepository,
     ShareUserRepository,
     TagRepository
 };
 use App\Repositories\Interfaces\{
     AppRepositoryInterface,
+    BudgetExpenseRepositoryInterface,
+    BudgetGoalRepositoryInterface,
+    BudgetIncomeRepositoryInterface,
+    BudgetProvisionRepositoryInterface,
+    BudgetRepositoryInterface,
+    CreditCardInvoiceExpenseDivisionRepositoryInterface,
+    CreditCardInvoiceExpenseRepositoryInterface,
+    CreditCardInvoiceFileRepositoryInterface,
+    CreditCardInvoiceRepositoryInterface,
+    CreditCardRepositoryInterface,
+    FinancingInstallmentRepositoryInterface,
+    FinancingRepositoryInterface,
+    FixExpenseRepositoryInterface,
     ProvisionRepositoryInterface,
     ShareUserRepositoryInterface,
     TagRepositoryInterface
@@ -24,9 +50,22 @@ class RepositoryServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(AppRepositoryInterface::class, AppRepository::class);
-        $this->app->bind(ShareUserRepositoryInterface::class, ShareUserRepository::class);
-        $this->app->bind(TagRepositoryInterface::class, TagRepository::class);
+        $this->app->bind(BudgetExpenseRepositoryInterface::class, BudgetExpenseRepository::class);
+        $this->app->bind(BudgetGoalRepositoryInterface::class, BudgetGoalRepository::class);
+        $this->app->bind(BudgetIncomeRepositoryInterface::class, BudgetIncomeRepository::class);
+        $this->app->bind(BudgetProvisionRepositoryInterface::class, BudgetProvisionRepository::class);
+        $this->app->bind(BudgetRepositoryInterface::class, BudgetRepository::class);
+        $this->app->bind(CreditCardInvoiceExpenseDivisionRepositoryInterface::class, CreditCardInvoiceExpenseDivisionRepository::class);
+        $this->app->bind(CreditCardInvoiceExpenseRepositoryInterface::class, CreditCardInvoiceExpenseRepository::class);
+        $this->app->bind(CreditCardInvoiceFileRepositoryInterface::class, CreditCardInvoiceFileRepository::class);
+        $this->app->bind(CreditCardInvoiceRepositoryInterface::class, CreditCardInvoiceRepository::class);
+        $this->app->bind(CreditCardRepositoryInterface::class, CreditCardRepository::class);
+        $this->app->bind(FinancingInstallmentRepositoryInterface::class, FinancingInstallmentRepository::class);
+        $this->app->bind(FinancingRepositoryInterface::class, FinancingRepository::class);
+        $this->app->bind(FixExpenseRepositoryInterface::class, FixExpenseRepository::class);
         $this->app->bind(ProvisionRepositoryInterface::class, ProvisionRepository::class);
+        $this->app->bind(TagRepositoryInterface::class, TagRepository::class);
+        $this->app->bind(ShareUserRepositoryInterface::class, ShareUserRepository::class);
     }
 
     /**
